87004a7236d4f713ccc04e5a8bccef58b7534da8
yuyang <songshengyuyang@sina.com>
2019-12-14 18:47:53 +0800

    delete sensitive information

--- login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect -f
-set user liyanrong
-set host lxslc6.ihep.ac.cn
-set password !qaz2wsx3edc
-set timeout 10
-spawn ssh $user@$host
-expect "*assword:*"
-send "$password\r"
-interact
